fd07b80bd2057026dd1a4a4c5737e5dfcc7854a2
Lachlan Enderlin <lendo1995@gmail.com>
2017-03-28 00:05:16 +1000

    Created and set up MEAN Application

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+const port = 3000;
+
+var app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Set Static folder where Angular will be stored
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(port , function() {
+    console.log('Server started on port ' + port);
+});
